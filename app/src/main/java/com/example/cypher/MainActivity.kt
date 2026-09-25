4be51d560aa81805e2e31efbf8dff40aa29315ef
Cesar Santamaria <cesantam@ualberta.ca>
2026-09-25 12:41:44 -0600

    Implemented colour field on class Shape

--- app/src/main/java/com/example/cypher/MainActivity.kt
+++ app/src/main/java/com/example/cypher/MainActivity.kt
@@ -48,7 +48,8 @@ fun GreetingPreview() {
 
 abstract class Shape(
     var x: Int,
-    var y: Int
+    var y: Int,
+    val colour: String = "blue"
 ){}
 
 class Square(x: Int, y: Int, var width: Int) : Shape(x,y)
